5bc69854e17846d1c905db22fb160254de980c66
dan dominan <ddominan12@gmail.com>
2021-05-15 10:04:04 +0300

    save2

--- 04_week_homework/mandatory.js
+++ 04_week_homework/mandatory.js
@@ -22,15 +22,27 @@ console.log(sum)
 // var max = Math.max(...numbers)
 // sau
 let max = numbers [0]
-for (let a = 1; a <numbers.length; a++) {
+for (let a = 0; a <numbers.length; a++) {
     if (numbers[a]>max) {
         max = numbers [a]
     }
 }
 console.log(max)
 
-// 5. hom many times a certain element appears in an array
+// 5. how many times a certain element appears in an array
 
-var array1 = [5,6,12,12,50,34,60]
+var array1 = [60,5,6,12,60,12,50,34,12,60,45,60,2]
 
+        // de cate ori apare un anumit element (12)
 
+var noTimes = array1.filter(function1)
+function function1(value, index, array){
+    return value == 12;
+}
+console.log(noTimes.length)
+
+        // exista vreun element care apare de mai multe ori?
+
+var nonUnique = array1.filter((x,y,z) => z.indexOf(x) !== y)
+nonUnique.sort((c,d) => c - d);
+console.log(nonUnique)
