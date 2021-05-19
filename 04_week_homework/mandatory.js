374c1e4e231da4efe2dc6209c2e6f9d8a6ebe3d7
dan dominan <ddominan12@gmail.com>
2021-05-19 16:50:36 +0300

    homework save

--- 04_week_homework/mandatory.js
+++ 04_week_homework/mandatory.js
@@ -1,15 +1,17 @@
 // 1. display 1-20
-
+var ex1=[]
 for (var i = 1; i <= 20; i++){
-    console.log(i)
+    ex1.push(i)
 }
+console.log(ex1)
 
 // 2. display odd numbers 1-20
-
+var ex2=[]
 for (var o = 1; o <= 20; o++){
     if((o%2) != 0 )
-    console.log(o)
+    ex2.push(o)
 }
+console.log(ex2)
 
 // 3. sum of the numbers in an array
 
@@ -31,9 +33,10 @@ console.log(max)
 
 // 5. how many times a certain element appears in an array
 
-var array1 = [60,5,6,12,60,12,50,34,12,60,45,60,2]
+var array1 = [60,5,6,12,60,12,50,34,1,60,45,60,12]
 
-        // de cate ori apare un anumit element (12)
+        // de cate ori apare un anumit element 
+        // ex: de cate ori apare nr 12?
 
 var noTimes = array1.filter(function1)
 function function1(value, index, array){
@@ -45,47 +48,22 @@ console.log(noTimes.length)
 
 var nonUnique = array1.filter((x,y,z) => z.indexOf(x) !== y)
 nonUnique.sort((c,d) => c - d);
+
 console.log(nonUnique)
 
 // 6. 
-//var rows = 4;
-//var cols = 4;
+var rows = 4;
+var cols = 4;
 
-for(f=1; f<=1; f++)
+for(f=1; f<=rows; f++)
 {
-    for (g=1; g<=2; g++)
+    for (g=1; g<=cols; g++)
     {
-        document.write("0 ");
-        document.write("1 ")
+     if (g%2 != 0 && f%2 != 0) {document.write("0 ")}
+     if (g%2 == 0 && f%2 != 0) {document.write("1 ")}
+     if (g%2 != 0 && f%2 == 0) {document.write("1 ")}
+     if (g%2 == 0 && f%2 == 0) {document.write("0 ")}
     }
     document.write("<br/>")
     
-}
-for(f=2; f<=2; f++)
-{
-    for(g=2; g<=3; g++)
-    {
-        document.write("1 ")
-        document.write("0 ")
-    }
-    document.write("<br/>")
-}
-for(f=1; f<=1; f++)
-{
-    for (g=1; g<=2; g++)
-    {
-        document.write("0 ");
-        document.write("1 ")
-    }
-    document.write("<br/>")
-    
-}
-for(f=2; f<=2; f++)
-{
-    for(g=2; g<=3; g++)
-    {
-        document.write("1 ")
-        document.write("0 ")
-    }
-    document.write("<br/>")
 }
--- 05_week/expresii.js
+++
@@ -1,6 +0,0 @@
-// expresii matematice
-var number1 = 10 - (20/20) * 3;
-
-// expresii logice
-
-var conditie = (10>2 && 20 === 212)
