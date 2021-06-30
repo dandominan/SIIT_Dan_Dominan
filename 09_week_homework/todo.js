87765211805c033d0da3d2affca881d77147d565
ddominan <ddominan12@gmail.com>
2021-06-30 21:08:16 +0300

    final save

--- 09_week_homework/todo.js
+++ 09_week_homework/todo.js
@@ -1,40 +1,94 @@
-async function getTodos(){
-    let getList = await fetch("http://localhost:3000/todos", {method: 'GET'})
+
+async function getTodos() {
+    let getList = await fetch("http://localhost:3000/todos", { method: 'GET' })
     return getList.json();
 }
 
-// getTodos();
-// console.log(getTodos())
+getTodos()
+    .then((toDos) => {
+        console.log(toDos)
+    })
 
-function createToDo(text){
-    let toDoContainer = document.getElementById("todo-container")
+async function displayTodos() {
+    let toDoContainer = document.getElementById('todo-container');
+    let toDoList = await getTodos();
+
+    toDoList.map((toDo) =>
+        toDoContainer.append(createToDo(toDo)))
+
+}
+
+displayTodos();
+
+
+function createToDo(toDoItem) {
+
     let todoItem = document.createElement('div')
-    let deleteBtn = document.createElement('button')
-    deleteBtn.innerText = 'x'
-    let todoText = document.createElement('h2')
-    todoText.innerText = text;
-    
-    todoItem.appendChild(todoText);
-    todoItem.appendChild(deleteBtn)
-    toDoContainer.appendChild(todoItem);
-    return toDoContainer
+    todoItem.setAttribute("id", toDoItem.id);
+    todoItem.innerHTML = `
+     <h2>${toDoItem.text}</h2>
+     <input type="checkbox" id="checkbox">
+     <button onClick="deleteToDo(${toDoItem.id})"> x </button>
+    `
+    let checkbox = todoItem.querySelector('#checkbox')
+    checkbox.addEventListener('change', function () {
+        console.log("merge")
+        toDoItem.checked = !toDoItem.checked;
+        console.log(toDoItem)
+        updateTodo(toDoItem);
+    })
+    return todoItem;
 }
 
 let addBtn = document.getElementById("add-btn");
-addBtn.addEventListener('click', function(){
+addBtn.addEventListener('click', async function () {
+    let toDoContainer = document.getElementById('todo-container')
     let inputText = document.getElementById("input-text");
-    createToDo(inputText.value);
-  
+    let todo = await addToDo(inputText.value);
+    toDoContainer.appendChild(createToDo(todo));
+
 })
 
-const createToDos = (toDos) => {
-    let toDoContainer = document.getElementById("todo-container")
-    let toDoItem = toDos.map((toDo) => createToDo(toDo.text))
-    // toDoContainer.appendChild(...toDoItem)
+async function addToDo(text) {
+    let newToDo = { text: text }
+    let response = await fetch('http://localhost:3000/todos',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newToDo)
+        })
+    let theToDo = await response.json();
+    return theToDo;
 }
 
-async function displayTodos(toDos){
-  let todoList = await getTodos();
-  let todoHtml = createToDos(todoList);
+async function updateTodo(todo) {
+    let response = await fetch('http://localhost:3000/todos/' + todo.id,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(todo)
+        })
+    let theToDo = await response.json();
+    return theToDo;
 }
-displayTodos();
+
+
+async function deleteToDo(id) {
+    let response = await fetch('http://localhost:3000/todos/' + id,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        })
+    let deletedToDo = await response.json();
+    let toDoParent = document.getElementById("todo-container")
+    let toDoRemoved = document.getElementById(deletedToDo.id)
+    toDoParent.removeChild(toDoRemoved)
+}
+
