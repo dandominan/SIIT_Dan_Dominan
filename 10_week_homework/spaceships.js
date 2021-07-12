f1d25f61f17d580d047ad0a7ec2db59cff2c2f42
ddominan <ddominan12@gmail.com>
2021-07-12 18:27:28 +0300

    final

--- 10_week_homework/spaceships.js
+++ 10_week_homework/spaceships.js
@@ -1,63 +1,113 @@
 class Spaceship {
-    constructor(){
+    constructor(color) {
         this.active = false;
+        this.color = color;
     }
-    createSpaceshipEl(){
+
+    createSpaceshipEl() {
         const imgEl = document.createElement('img');
-        imgEl.src = "blue-spaceship.png";
+        imgEl.src = this.color + "-spaceship.png";
         imgEl.style.width = "100px";
         imgEl.style.display = "block";
         imgEl.style.marginTop = "10px"
         return imgEl;
     }
-    display(parentEl){
+    display(parentEl) {
         this.spaceshipEl = this.createSpaceshipEl();
         this.spaceshipEl.style.position = "relative";
-        this.spaceshipEl.style.left = "0px"
+        this.spaceshipEl.style.left = "10px"
+        this.spaceshipEl.style.top = "10px"
         parentEl.append(this.spaceshipEl);
         this.registerClickEvent();
     }
 
-    registerClickEvent(){
-        this.spaceshipEl.addEventListener("click", () =>{
+    registerClickEvent() {
+        this.spaceshipEl.addEventListener("click", () => {
             this.active = !this.active;
             this.spaceshipEl.style.backgroundColor = this.active ? "black" : "white";
         });
     }
-    moveRight(){
-        if (this.active){
+    moveRight() {
+        if (this.active) {
             const left = parseInt(this.spaceshipEl.style.left, 10);
             this.spaceshipEl.style.left = left + 5 + "px";
-        }}
-    moveLeft(){
-        if(this.active){
+        }
+    }
+    moveLeft() {
+        if (this.active) {
             const left = parseInt(this.spaceshipEl.style.left, 10);
             this.spaceshipEl.style.left = left - 5 + "px";
-    }}
+        }
+    }
+    moveUp() {
+        if (this.active) {
+            const top = parseInt(this.spaceshipEl.style.top, 10);
+            this.spaceshipEl.style.top = top - 5 + "px";
+        }
+    }
+    moveDown() {
+        if (this.active) {
+            const top = parseInt(this.spaceshipEl.style.top, 10);
+            this.spaceshipEl.style.top = top + 5 + "px";
+        }
+    }
 }
 
-const spaceship1 = new Spaceship();
-const spaceship2 = new Spaceship();
 
-const spaceList = [spaceship1, spaceship2];
+const spaceList = [];
 spaceList.forEach((spaceship) => spaceship.display(document.body));
 
-document.addEventListener("keydown", function(event){
+document.addEventListener("keydown", function (event) {
     if (event.code === "ArrowRight") {
-        spaceList.forEach((spaceship)=>{
+        spaceList.forEach((spaceship) => {
             spaceship.moveRight();
         })
     }
     if (event.code === "ArrowLeft") {
         spaceList.forEach((spaceship) => {
             spaceship.moveLeft();
-        } )
+        })
+    }
+    if (event.code === "ArrowUp") {
+        spaceList.forEach((spaceship) => {
+            spaceship.moveUp();
+        })
+    }
+    if (event.code === "ArrowDown") {
+        spaceList.forEach((spaceship) => {
+            spaceship.moveDown();
+        })
     }
 })
 
 const generateBtn = document.getElementById('generate');
-generateBtn.addEventListener("click", function(){
-    const spaceship = new Spaceship();
-    spaceList.push(spaceship);
-    spaceship.display(document.body);
+let clicks = 0;
+
+generateBtn.addEventListener("click", function () {
+
+    clicks++;
+    console.log(clicks);
+//  debugger;
+    if (clicks === 1) {
+        const spaceshipNew = new Spaceship("green");
+        spaceList.push(spaceshipNew);
+        spaceshipNew.display(document.body);
+    }
+    if(clicks === 2){
+        const spaceshipNew = new Spaceship("blue");
+        spaceList.push(spaceshipNew);
+        spaceshipNew.display(document.body);
+    }
+    if(clicks === 3){
+        const spaceshipNew = new Spaceship("red");
+        spaceList.push(spaceshipNew);
+        spaceshipNew.display(document.body);
+    }
+    if (clicks === 4) {
+        clicks = 1;
+        const spaceshipNew = new Spaceship("green");
+        spaceList.push(spaceshipNew);
+        spaceshipNew.display(document.body);
+    }
 });
+
